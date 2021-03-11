5da907c537aa42c25061b6cb9487e533ccad905a
koyuboy <35155252+koyuboy@users.noreply.github.com>
2021-03-11 21:17:32 +0300

    11.03.2021  angular oluşturuldu bootstrap eklendi

---
+++ src/app/components/brand/brand.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brand',
+  templateUrl: './brand.component.html',
+  styleUrls: ['./brand.component.css'],
+})
+export class BrandComponent implements OnInit {
+  brand1 = { brandId: 1, brandName: 'BMV' };
+  brand2 = { brandId: 2, brandName: 'Mercedes' };
+  brand3 = { brandId: 2, brandName: 'Tesla' };
+
+  brands = [this.brand1, this.brand2, this.brand3];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
