1fb8a24342b2a4394e4b20ce51d09724af828535
koyuboy <35155252+koyuboy@users.noreply.github.com>
2021-03-11 21:24:21 +0300

    d

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class BrandComponent implements OnInit {
   brand1 = { brandId: 1, brandName: 'BMV' };
   brand2 = { brandId: 2, brandName: 'Mercedes' };
-  brand3 = { brandId: 2, brandName: 'Tesla' };
-
+  brand3 = { brandId: 3, brandName: 'Tesla' };
+  brand4 = { brandId: 4, brandName: 'Nissan' };
   brands = [this.brand1, this.brand2, this.brand3];
 
   constructor() {}
