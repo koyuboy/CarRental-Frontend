a3ea513a5726b0b7867941e9dc05ee50a9627404
koyuboy <35155252+koyuboy@users.noreply.github.com>
2021-03-11 21:42:59 +0300

    s

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -9,7 +9,7 @@ export class BrandComponent implements OnInit {
   brand1 = { brandId: 1, brandName: 'BMV' };
   brand2 = { brandId: 2, brandName: 'Mercedes' };
   brand3 = { brandId: 3, brandName: 'Tesla' };
-  brand4 = { brandId: 4, brandName: 'Toyota' };
+  brand4 = { brandId: 4, brandName: 'Mazda' };
   brands = [this.brand1, this.brand2, this.brand3];
 
   constructor() {}
